39b7f9617c9bbdf52926542c7693805bc2434e6f
olga-prog <oo20_03@mail.ru>
2025-04-02 01:23:29 +0300

    new

--- script/cheakout.js
+++ script/cheakout.js
@@ -3,17 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
   const bookData = JSON.parse(localStorage.getItem("BookforBuy"));
   const userData = JSON.parse(localStorage.getItem("user"));
   const selectedBookContent = document.querySelector(".selected-book-content");
-  alert(userData)
-
+  let tgCheak = window.Telegram.WebApp; 
+ 
   if (!bookData) {
     selectedBookContent.innerHTML = "<p>No book selected.</p>";
     return; // Выходим из функции, если нет данных о книге
   }
 
-  if (!userData || !userData.telegram_id) {
-    selectedBookContent.innerHTML = "<p>No user data or telegram_id found.  Please log in again.</p>";
-    return; // Выходим из функции, если нет данных о пользователе
-  }
 
   selectedBookContent.innerHTML = `
     <h2>${bookData.title}</h2>
@@ -25,22 +21,21 @@ document.addEventListener("DOMContentLoaded", function() {
     event.preventDefault(); // Предотвращаем отправку формы (если она есть)
 
     const data = {
-      name: bookData.name, // Отправляем "name", а не "book_name"
+      name: bookData.name, 
       price: bookData.price,
-      user_id: userData.telegram_id,
     };
 
     console.log("Sending data to bot:", data); // Добавляем логирование
 
-    Telegram.WebApp.sendData(JSON.stringify(data));
+    tgCheak.sendData(JSON.stringify(data));
 
-    Telegram.WebApp.onEvent('web_app_data', (event) => {
+    tgCheak.onEvent('web_app_data', (event) => {
       try {
         console.log("Received data from bot:", event.data); // Добавляем логирование
 
         const responseData = JSON.parse(event.data);
         if (responseData && responseData.invoice_link) { // Проверяем, что объект responseData существует и содержит invoice_link
-          Telegram.WebApp.openInvoice(responseData.invoice_link);
+          tgCheak.openInvoice(responseData.invoice_link);
         } else {
           console.error('No invoice link received from bot or invalid format:', responseData); // Добавляем логирование
           alert('Не удалось создать счет. Попробуйте позже.');
