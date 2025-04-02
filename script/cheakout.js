7504ec30108d484f02a6122c7b2dd936f3eb80e5
olga-prog <oo20_03@mail.ru>
2025-04-02 20:15:39 +0300

    add34

--- script/cheakout.js
+++ script/cheakout.js
@@ -19,8 +19,9 @@ document.addEventListener("DOMContentLoaded", function() {
   const buttonSudmit = document.getElementById("button-sudmit");
   buttonSudmit.addEventListener("click", function(event) {
     event.preventDefault(); 
-    alert('file:', bookData.ebook_file)
-    let filename = bookData.ebook_file.split('/')[-1]
+    const file_book = JSON.stringify(bookData.ebook_file)
+    alert('file:', file_book)
+    let filename = file_book.split('/')[-1]
     alert(filename)
     //books/ebooks/Reyli_Hroniki_mafii_Rozhdyonnye_v_krovi_1_Svyazannye_chestyu.epub
     const data = {
